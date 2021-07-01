c5491e1457f297928f4cd8abc1521bb8dbb2d9f4
Abhishek Yadav <abhishek26321@gmail.com>
2021-07-02 00:32:44 +0530

    Added files for session limit logic and config

---
+++ session-limiter/src/main/java/com/itsabhishek/keycloak/SessionLimitAuthenticator.java
@@ -0,0 +1,41 @@
+package com.itsabhishek.keycloak;
+
+
+import org.jboss.logging.Logger;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.models.*;
+
+public class SessionLimitAuthenticator implements Authenticator{
+
+    @Override
+    public void authenticate(AuthenticationFlowContext authenticationFlowContext) {
+
+    }
+
+    @Override
+    public void action(AuthenticationFlowContext authenticationFlowContext) {
+
+    }
+
+    @Override
+    public boolean requiresUser() {
+        return false;
+    }
+
+    @Override
+    public boolean configuredFor(KeycloakSession keycloakSession, RealmModel realmModel, UserModel userModel) {
+        return false;
+    }
+
+    @Override
+    public void setRequiredActions(KeycloakSession keycloakSession, RealmModel realmModel, UserModel userModel) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
