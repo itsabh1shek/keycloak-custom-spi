9b05fe6a8d8eded736048085d8d10aa258700760
Abhishek Yadav <abhishek26321@gmail.com>
2021-07-03 20:02:23 +0530

    Added file for constants

---
+++ session-limiter/src/main/java/com/itsabhishek/keycloak/Constants.java
@@ -0,0 +1,11 @@
+package com.itsabhishek.keycloak;
+
+public class Constants {
+    public static final String DISPLAY_TYPE = "User Session Limiter";
+    public static final String USER_REALM_LIMIT = "userRealmLimit";
+    public static final String ACTION = "action";
+    public static final String DENY_NEW_SESSION = "Deny new session";
+    public static final String TERMINATE_OLDEST_SESSION = "Terminate oldest session";
+    public static final String LIMIT_USER_SESSION = "limitUserSession";
+
+}
