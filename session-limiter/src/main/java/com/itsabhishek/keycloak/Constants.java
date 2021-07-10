07eb2915cb9afefca001f45dfec8da09fa56978c
Abhishek Yadav <abhishek26321@gmail.com>
2021-07-10 16:46:15 +0530

    Added new constants

--- session-limiter/src/main/java/com/itsabhishek/keycloak/Constants.java
+++ session-limiter/src/main/java/com/itsabhishek/keycloak/Constants.java
@@ -3,9 +3,11 @@ package com.itsabhishek.keycloak;
 public class Constants {
     public static final String DISPLAY_TYPE = "User Session Limiter";
     public static final String USER_REALM_LIMIT = "userRealmLimit";
-    public static final String USER_REALM_LIMIT_LABEL = "Maximum concurrent sessions for a user in a realm";
+    public static final String USER_REALM_LIMIT_HELP_TEXT = "Maximum concurrent sessions for a user in a realm";
+    public static final String USER_REALM_LIMIT_LABEL = "Maximum concurrent sessions";
     public static final String ACTION = "action";
-    public static final String ACTION_LABEL = "Action when user session limit is exceeded";
+    public static final String ACTION_LABEL = "Action";
+    public static final String ACTION_LABEL_HELP_TEXT = "Action when user session limit is exceeded";
     public static final String DENY_NEW_SESSION = "Deny new session";
     public static final String TERMINATE_OLDEST_SESSION = "Terminate oldest session";
     public static final String LIMIT_USER_SESSION = "limitUserSession";
